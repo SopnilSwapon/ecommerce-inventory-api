74a95c81515f43819e43d5b7345887587af0e464
SopnilSwapon <sopnilswapon1@gmail.com>
2025-09-21 12:38:17 +0600

    Add NestJS modules for dependency injection

---
+++ src/products/products.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsService } from './products.service';
+import { ProductsController } from './products.controller';
+import { Product } from './entities/product.entity';
+import { ProductRepository } from './repositories/product.repository';
+import { CategoriesModule } from '../categories/categories.module';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Product]), CategoriesModule],
+  controllers: [ProductsController],
+  providers: [ProductsService, ProductRepository],
+})
+export class ProductsModule {}
